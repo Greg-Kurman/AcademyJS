a20b2662d0f0cbcda876a4cd4a83c049f5683485
greg <grigroiykurman@gmail.com>
2022-09-08 14:14:42 +0300

    Второй урок Академия Дж

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-let num = 3278;
 
-alert((3*2*7*8)**3);
+let money = prompt('ваш бюджет')
+let name = prompt('название вашего магазина ?')
+
+let time = 12
+
+
+
+let mainList = {
+    budget: money,
+    shopName: name,
+    shopProducts: [],
+    employers: {},
+    open: false
+}
+
+
+for (let i = 0; i < 5; i++) {
+    let a = prompt('какой товар будем продавать?')
+
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && a != '' && a.length < 50) {
+        console.log('Данные внесены')
+        mainList.shopProducts[i] = a
+    } else {
+
+    }
+}
+
+if( time >8 && time <18) {
+    console.log('магазин работает')
+} else {
+    console.log('магазин не работает')
+}
+
+console.log(mainList)
+
+
